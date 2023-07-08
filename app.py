c034cccbfcab515a019df42ef42375bf02b86b9a
Karthik Soman <karthi.soman@gmail.com>
2023-07-07 18:20:31 -0700

    adding requirements file

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ FONT_SIZE = 18
 LEGEND_SIZE = 12
 
 
-with open("../bcmm_compounds_all_bacteria_with_proximity_pvalue.pickle", "rb") as f:
+with open("data/bcmm_compounds_all_bacteria_with_proximity_pvalue.pickle", "rb") as f:
     data = pickle.load(f)
 compound_names = list(data.keys())[:-2]
 compounds_to_remove = ['diacetamate',
